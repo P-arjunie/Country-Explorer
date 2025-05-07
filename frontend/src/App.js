c5f28fadf193ac6731527c6ab4cbce2afa85212c
Pasindi Arjunie <154939837+P-arjunie@users.noreply.github.com>
2025-05-07 12:25:59 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -298,6 +298,7 @@ export default function App() {
         onShowFavorites={handleShowFavorites}
         theme={theme}
         toggleTheme={toggleTheme}
+        onShowLogin={handleShowLogin}
       />
       
       {showFavorites && isLoggedIn && (
